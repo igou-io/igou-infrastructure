411dee5ba8d5196a11b88e907c7459c941502589
David Igou <igou.david@gmail.com>
2022-12-29 16:26:16 -0500

    main.tf formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,17 @@
 // Networking
 
+module "vpc_main" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                 = "wireguard"
+  cidr                 = var.vpc_cidr
+  public_subnets       = var.vpc_public_subnets
+  azs                  = var.vpc_azs
+  enable_dns_hostnames = true
+}
+
 resource "aws_eip" "wireguard" {
   instance = aws_instance.wireguard.id
-
   tags = {
     Name : "wireguard"
   }
@@ -12,21 +21,18 @@ resource "aws_eip" "wireguard" {
 resource "aws_security_group" "kubernetes_ingress" {
   name   = "kubernetes_ingress"
   vpc_id = module.vpc_main.vpc_id
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     security_groups = [aws_security_group.wireguard.id]
   }
-
   ingress {
     from_port   = 8472
     to_port     = 8472
     protocol    = "udp"
     security_groups = [aws_security_group.wireguard.id]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -38,28 +44,24 @@ resource "aws_security_group" "kubernetes_ingress" {
 resource "aws_security_group" "loadbalancer" {
   name   = "loadbalancer"
   vpc_id = module.vpc_main.vpc_id
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     security_groups = [aws_security_group.wireguard.id]
   }
-
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -68,25 +70,21 @@ resource "aws_security_group" "loadbalancer" {
   }
 }
 
-
 resource "aws_security_group" "wireguard" {
   name   = "wireguard"
   vpc_id = module.vpc_main.vpc_id
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
   ingress {
     from_port   = 51820
     to_port     = 51820
     protocol    = "udp"
     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -122,23 +120,11 @@ resource "aws_security_group_rule" "allow_6443_from_kubernetes_ingress" {
     source_security_group_id = aws_security_group.kubernetes_ingress.id
 }
 
-
-module "vpc_main" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name                 = "wireguard"
-  cidr                 = var.vpc_cidr
-  public_subnets       = var.vpc_public_subnets
-  azs                  = var.vpc_azs
-  enable_dns_hostnames = true
-}
-
 resource "aws_route" "gateway_route" {
   route_table_id         = module.vpc_main.public_route_table_ids[0]
   destination_cidr_block = "10.10.1.0/24"
   network_interface_id   = aws_instance.wireguard.primary_network_interface_id
 }
-
 
 // Instances
 
@@ -158,16 +144,13 @@ resource "aws_instance" "loadbalancer" {
   root_block_device {
     volume_size = var.loadbalancer_instance_disk_size
   }
-
   lifecycle {
     ignore_changes = [ami]
   }
-
   tags = {
     role = "loadbalancer"
     Name = "loadbalancer"
   }
-
 }
 
 resource "aws_instance" "kubernetes_ingress" {
@@ -181,7 +164,6 @@ resource "aws_instance" "kubernetes_ingress" {
   root_block_device {
     volume_size = var.kubernetes_ingress_instance_disk_size
   }
-
   lifecycle {
     ignore_changes = [ami]
   }
@@ -202,7 +184,6 @@ resource "aws_instance" "wireguard" {
   root_block_device {
     volume_size = var.gateway_instance_disk_size
   }
-
   lifecycle {
     ignore_changes = [ami]
   }
@@ -211,7 +192,3 @@ resource "aws_instance" "wireguard" {
     Name = "wireguard_endpoint"
   }
 }
-
-
-
-
