efe24e161904aa323313a72da89c3b5813208956
David Igou <igou.david@gmail.com>
2024-04-18 22:52:55 -0400

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = "~> 1.2"
   backend "s3" {}
   required_providers {
-    "hashicorp/aws" = {
-      version = "4.48.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
     }
   }
 }
